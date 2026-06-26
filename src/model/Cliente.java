3c1548325ca305d8205da534b194c2ef1e3b824f
SalomãoTK96 <patrick26franca@hotmail.com>
2026-06-26 20:02:04 -0300

    cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,5 +1,6 @@
 package model;
 
+// representa um cliente do sistema de passagens
 public class Cliente {
 
     private int    idCliente;
@@ -8,8 +9,10 @@ public class Cliente {
     private String email;
     private String dataNascimento;
 
+    // construtor vazio, sem daddos
     public Cliente() {}
 
+    // construtor completo usado ao buscar dados do banco
     public Cliente(int idCliente, String nome, String cpf, String email, String dataNascimento) {
         this.idCliente      = idCliente;
         this.nome           = nome;
@@ -17,6 +20,8 @@ public class Cliente {
         this.email          = email;
         this.dataNascimento = dataNascimento;
     }
+
+    // getters e setters para acessar e modificar os atributos
 
     public int    getIdCliente()              { return idCliente; }
     public void   setIdCliente(int i)         { this.idCliente = i; }
@@ -29,6 +34,7 @@ public class Cliente {
     public String getDataNascimento()         { return dataNascimento; }
     public void   setDataNascimento(String d) { this.dataNascimento = d; }
 
+    // exibe os dados do cliente
     @Override
     public String toString() {
         return "Cliente{id=" + idCliente + ", nome='" + nome + "', cpf='"
